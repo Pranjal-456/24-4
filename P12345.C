167e8b0017e1b698a5b45f536bd7d5671a27e687
Pranjal-456 <131869517+Pranjal-456@users.noreply.github.com>
2023-05-04 15:18:34 +0530

    Add files via upload

---
+++ P12345.C
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+
+ int a,b,c;
+ clrscr();
+ a=12;
+ b=34;
+ c=a;
+ a=b;
+ b=c;
+ printf("value of a=%d",a);
+ printf("\nvalue of b=%d",b);
+
+
+
+
+
+
+
+
+		getch();
+
+ }
